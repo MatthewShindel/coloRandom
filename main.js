bf87c6f8be80211fa9a168a4fc3e6a15e110c7c2
Dana Zack <danaezack@gmail.com>
2023-10-31 15:08:50 -0600

    Feat: Add and style newPalette button Co-authored-by: Matthew Shindel <matthew@shindel.com> Co-authored-by: Jamie Francisco <0jamiefrancisco@gmail.com>

---
+++ main.js
@@ -0,0 +1,7 @@
+var box1 = document.querySelector('#box1');
+var box2 = document.querySelector('#box2');
+var box3 = document.querySelector('#box3');
+var box4 = document.querySelector('#box4');
+var box5 = document.querySelector('#box5');
+
+var newPalette = document.querySelector('#new-palette');
