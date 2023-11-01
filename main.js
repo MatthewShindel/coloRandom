118b6d97ed2783e325ac8ec5ae54392ab2356c2e
Jamie Francisco <0jamiefrancisco@gmail.com>
2023-11-01 11:10:48 -0700

    Refactor: Clean class and ID names, restructure HTML  Co-authored-by: Matthew Shindel <matthew@shindel.com>  Co-authored-by: Dana Zack <danaezack@gmail.com>

--- main.js
+++ main.js
@@ -1,77 +1,53 @@
+// Variables
 var box1 = document.querySelector('#box1');
 var box2 = document.querySelector('#box2');
 var box3 = document.querySelector('#box3');
 var box4 = document.querySelector('#box4');
 var box5 = document.querySelector('#box5');
-var color1 = document.querySelector('#color1');
-var color2 = document.querySelector('#color2');
-var color3 = document.querySelector('#color3');
-var color4 = document.querySelector('#color4');
-var color5 = document.querySelector('#color5');
-
-var newPalette = document.querySelector('#new-palette');
-
+var hex1 = document.querySelector('#hex1');
+var hex2 = document.querySelector('#hex2');
+var hex3 = document.querySelector('#hex3');
+var hex4 = document.querySelector('#hex4');
+var hex5 = document.querySelector('#hex5');
+var newPalette = document.querySelector('#newPalette');
 var currentPalette = [];
 
-window.addEventListener('load', function() {
-    generateRandomPalette();
-    updatePalette(currentPalette)
+// Event Listeners
+window.addEventListener('load', function () {
+	generateRandomPalette();
+	updatePalette(currentPalette)
 });
 
-newPalette.addEventListener('click', function() {
-generateRandomPalette();
-updatePalette(currentPalette)
+newPalette.addEventListener('click', function () {
+	generateRandomPalette();
+	updatePalette(currentPalette)
 });
 
-
-
+// Functions
 function generateRandomColor() {
-    var characters = "0123456789ABCDEF";
-    var lead = "#";
-    for (var i = 0; i < 6; i++) {
-        lead += characters[Math.floor(Math.random() * 16)];
-    }
-    return lead;
+	var characters = "0123456789ABCDEF";
+	var lead = "#";
+	for (var i = 0; i < 6; i++) {
+		lead += characters[Math.floor(Math.random() * 16)];
+	}
+	return lead;
 }
 
 function generateRandomPalette() {
-    currentPalette = [];
-    for (var i = 0; i < 5; i++) {
-        currentPalette.push(generateRandomColor());
-    }
-    return currentPalette;
+	currentPalette = [];
+	for (var i = 0; i < 5; i++) {
+		currentPalette.push(generateRandomColor());
+	}
+	return currentPalette;
 }
 
-
 function updatePalette(array) {
-
-    // create variable to store string of currentboxnumber
-    // target the element by ID to look for an ID that matches that string
-    for (var i = 0; i < array.length; i++) {
-        var boxNum = "box" + (i + 1);
-        var colorNum = "color" + (i + 1);
-        var box = document.getElementById(boxNum);
-        var color = document.getElementById(colorNum);
-        box.style.backgroundColor = array[i];
-        color.innerText = array[i];
-
-        // boxNum += (i + 1); 
-        
-        // if (boxNum === 'box1') {
-        //     box1.style.backgroundColor = array[i];
-        //     color1.innerText = array[i];
-        // } else if (boxNum === 'box2') {
-        //     box2.style.backgroundColor = array[i];
-        //     color2.innerText = array[i];
-        // } else if (boxNum === 'box3') {
-        //     box3.style.backgroundColor = array[i];
-        //     color3.innerText = array[i];
-        // } else if (boxNum === 'box4') {
-        //     box4.style.backgroundColor = array[i];
-        //     color4.innerText = array[i];
-        // } else if (boxNum === 'box5') {
-        //     box5.style.backgroundColor = array[i];
-        //     color5.innerText = array[i];
-        // }  
-    }
+	for (var i = 0; i < array.length; i++) {
+		var boxNum = "box" + (i + 1);
+		var hexNum = "hex" + (i + 1);
+		var box = document.getElementById(boxNum);
+		var hex = document.getElementById(hexNum);
+		box.style.backgroundColor = array[i];
+		hex.innerText = array[i];
+	}
 }
